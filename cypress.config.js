6e2a5f5b1a9b525489634bb375d996ca149d4958
jatsala <jatsalaberria@gmail.com>
2023-11-03 11:51:04 -0400

    fix cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,16 @@
 const { defineConfig } = require("cypress");
 
+const baseUrlProd = 'https://jats-fb-shopping.web.app/';
+const baseUrlDev = 'http://localhost:3000';
+
 module.exports = defineConfig({
   e2e: {
 
-    baseUrl: 'http://localhost:3000',
+    baseUrl: process.env.NODE_ENV === 'production' ? baseUrlProd : baseUrlDev
+  }
 
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
-  },
+  // setupNodeEvents(on, config) {
+  //   // implement node event listeners here
+  // },
+},
 });
